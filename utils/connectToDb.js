1d4ece393f725286be595630834d0f9199d1d486
czke33 <czke33@gmail.com>
2024-09-11 16:17:16 +0300

    backend with auth problems

---
+++ utils/connectToDb.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const colors = require("colors");
+require("dotenv").config();
+
+
+async function connectToDb() {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(colors.bgGreen.italic.bold("Database connection successful!"));
+  } catch (error) {
+    console.error(colors.red.italic.bold(error));
+    process.exit(1);
+  }
+}
+
+module.exports = connectToDb;
